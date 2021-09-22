105b60251ed16393617c2ac9dbae2599a5358cb1
York Schickl <ys@feinheit.ch>
2021-09-22 14:56:06 +0200

    actually use app settings to override defaults

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,5 @@
+# 0.0.2 (2021-09-22)
+- Readme
+- Actually use app settings to override default panel configuration
+
 # 0.0.1 (2021-08-17)
--- feincms3_cookiecontrol/templatetags/feincms3_cookiecontrol.py
+++ feincms3_cookiecontrol/templatetags/feincms3_cookiecontrol.py
@@ -1,8 +1,9 @@
 from itertools import chain
 
 from django import template
+from django.conf import settings
 from django.core.cache import cache
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import get_language, gettext_lazy as _
 
 from feincms3_cookiecontrol.models import CookieCategory
 
@@ -34,14 +35,15 @@ CACHE_TIMEOUT = 60 * 60 * 24
 
 @register.inclusion_tag("feincms3_cookiecontrol/panel.html")
 def feincms3_cookiecontrol_panel(page):
-    # cache is page specific bc of ancestor configuration
-    CACHE_KEY = f"feincms3_cookiecontrol_settings_{page.id}"
+    CACHE_KEY = f"feincms3_cookiecontrol_settings_{get_language()}"
 
     panel = cache.get(CACHE_KEY)
     if not panel:
+        setup = COOKIECONTROL_PANEL_DEFAULTS
+        setup.update(getattr(settings, "COOKIECONTROL", None))
         categories = CookieCategory.objects.prefetch_related("cookiescript_set")
         panel = {
-            **COOKIECONTROL_PANEL_DEFAULTS,
+            **setup,
             "categories": {t.name: t.serialize() for t in categories},
             "cookies": {
                 t.name: t.serialize()
@@ -52,12 +54,10 @@ def feincms3_cookiecontrol_panel(page):
         }
         cache.set(CACHE_KEY, panel, timeout=CACHE_TIMEOUT)
 
-    """
-    only show revoke button on legal_page
-    """
-    if panel["legalPage"] and not (page.id == panel["legalPage"]):
+    # only show revoke button on legal_page
+    if panel["legalPage"] and not (
+        panel["legalPage"] in [p.id for p in page.translations()]
+    ):
         panel.pop("revoke")
-
-    panel.pop("legalPage")
 
     return {"panel": panel}
