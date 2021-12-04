ec01e6640655f59f82dbfba17e0604df3621adc6
Matthias Kestenholz <mk@feinheit.ch>
2021-12-04 18:01:52 +0100

    Add unittests for the migration modules system check and for cookie script validations

--- tests/testapp/test_cookiecontrol.py
+++ tests/testapp/test_cookiecontrol.py
@@ -1,11 +1,15 @@
 import types
 
 from django import test
+from django.core.exceptions import ValidationError
 from django.template import Context, Template
+from django.test.utils import override_settings
+
+from feincms3_cookiecontrol.checks import check_settings
+from feincms3_cookiecontrol.models import CookieCategory, CookieScript
 
 
 # from .models import Model
-# from django.test.utils import override_settings
 # from django.utils.functional import lazy
 
 
@@ -32,3 +36,45 @@ class CookieControlTest(test.TestCase):
 
     def test_correct_setup_for_active_language(self):
         pass
+
+    @override_settings(MIGRATION_MODULES={})
+    def test_missing_migration_modules(self):
+        errors = check_settings(None)
+        self.assertEqual(
+            [error.id for error in errors],
+            ["feincms3_cookiecontrol.E001"],
+        )
+
+    def test_erroneous_scripts(self):
+        category = CookieCategory.objects.create()
+
+        # No exception
+        CookieScript(
+            category=category,
+            name="script-name",
+            inject_if="",
+            inject_else="",
+        ).full_clean()
+
+        with self.assertRaises(ValidationError) as cm:
+            CookieScript(
+                category=category,
+                name="script-name",
+                inject_if="function(){}",
+                inject_else="function(){}",
+            ).full_clean()
+
+        self.assertEqual(
+            [m.message for m in cm.exception.error_dict["inject_if"]],
+            [
+                "This doesn't look right. Please start with a HTML tag"
+                " (e.g. <script>, <div>)."
+            ],
+        )
+        self.assertEqual(
+            [m.message for m in cm.exception.error_dict["inject_else"]],
+            [
+                "This doesn't look right. Please start with a HTML tag"
+                " (e.g. <script>, <div>)."
+            ],
+        )
