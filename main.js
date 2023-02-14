08a5a1d170adb4b97924a816bc646bd985ae73e3
Matthias Kestenholz <mk@feinheit.ch>
2023-02-14 13:08:11 +0100

    Code golf the banner

--- main.js
+++ main.js
@@ -5,270 +5,263 @@
 
 import "./main.css"
 
-// eslint-disable-next-line no-extra-semi
-;(function () {
-  const cookieName = "f3cc",
-    settings =
-      window.f3ccData ||
-      JSON.parse(document.getElementById("f3cc-data").textContent),
-    injectedScripts = {},
-    providerKey = "f3cc-embed-providers"
-  let mainElement, banner, modify
+const qs = (selector, node = document) => node.querySelector(selector)
 
-  function crel(tagName, attributes = null, children = []) {
-    const dom = document.createElement(tagName)
-    if (attributes) {
-      for (let [name, value] of Object.entries(attributes)) {
-        if (name.startsWith("data-")) dom.setAttribute(name, value)
-        else dom[name] = value
-      }
+const cookieName = "f3cc",
+  settings = window.f3ccData || JSON.parse(qs("#f3cc-data").textContent),
+  injectedScripts = {},
+  providerKey = "f3cc-embed-providers"
+let mainElement, banner, modify
+
+function crel(tagName, attributes = null, children = []) {
+  const dom = document.createElement(tagName)
+  if (attributes) {
+    for (let [name, value] of Object.entries(attributes)) {
+      if (name.startsWith("data-")) dom.setAttribute(name, value)
+      else dom[name] = value
     }
-    dom.append(...children)
-    return dom
+  }
+  dom.append(...children)
+  return dom
+}
+
+function renderBanner() {
+  if (banner) {
+    show(banner)
+    return
   }
 
-  function renderBanner() {
-    if (banner) {
-      show(banner)
-      return
-    }
-
-    const content = [
-      crel("div", { className: "f3cc-title", textContent: settings.heading }),
-      crel("div", {
-        className: "f3cc-description",
-        textContent: settings.description,
-      }),
-    ]
-    if (settings.privacyPolicyURL) {
-      content[1].append(
-        crel("br"),
-        crel("a", {
-          textContent: settings.privacyPolicyTitle,
-          href: settings.privacyPolicyURL,
-        })
-      )
-    }
-    const buttons = [
+  const content = [
+    crel("div", { className: "f3cc-title", textContent: settings.heading }),
+    crel("div", {
+      className: "f3cc-description",
+      textContent: settings.description,
+    }),
+  ]
+  if (settings.privacyPolicyURL) {
+    content[1].append(
+      crel("br"),
       crel("a", {
-        className: "f3cc-button accept",
-        textContent: settings.buttonAccept,
-        onclick: onAcceptClick,
-      }),
-      crel("a", {
-        className: "f3cc-button reject",
-        textContent: settings.buttonReject,
-        onclick: onRejectClick,
-      }),
-    ]
-
-    banner = crel("div", { className: "f3cc f3cc-banner" }, [
-      crel("div", { className: "f3cc-container" }, [
-        crel("div", { className: "f3cc-content" }, content),
-        crel("div", { className: "f3cc-buttons" }, buttons),
-      ]),
-    ])
-
-    mainElement.append(banner)
-  }
-
-  function renderModify() {
-    if (modify) {
-      show(modify)
-      return
-    }
-
-    let existing
-    if ((existing = document.querySelector(".f3cc-modify"))) {
-      existing.addEventListener("click", (e) => {
-        e.preventDefault()
-        renderBanner()
+        textContent: settings.privacyPolicyTitle,
+        href: settings.privacyPolicyURL,
       })
-      return
-    }
+    )
+  }
+  const buttons = [
+    crel("a", {
+      className: "f3cc-button accept",
+      textContent: settings.buttonAccept,
+      onclick: onAcceptClick,
+    }),
+    crel("a", {
+      className: "f3cc-button reject",
+      textContent: settings.buttonReject,
+      onclick: onRejectClick,
+    }),
+  ]
 
-    const ppu = settings.privacyPolicyURL
-    const loc = window.location
-    if (
-      settings.buttonModify &&
-      (!ppu || ppu == `${loc.protocol}//${loc.host}${loc.pathname}`)
-    ) {
-      modify = crel("a", {
-        className: "f3cc-button modify",
-        textContent: settings.buttonModify,
-        onclick: (e) => {
-          e.preventDefault()
-          hide(modify)
-          renderBanner()
-        },
-      })
-      mainElement.append(modify)
-    }
+  banner = crel("div", { className: "f3cc f3cc-banner" }, [
+    crel("div", { className: "f3cc-container" }, [
+      crel("div", { className: "f3cc-content" }, content),
+      crel("div", { className: "f3cc-buttons" }, buttons),
+    ]),
+  ])
+
+  mainElement.append(banner)
+}
+
+function renderModify() {
+  if (modify) {
+    show(modify)
+    return
   }
 
-  function setCookie(value) {
-    let cookie = `${cookieName}=${value};max-age=31536000;path=/;sameSite=Strict`
-    if (settings.domain) {
-      cookie += `;domain=${settings.domain}`
-    }
-    document.cookie = cookie
-  }
-
-  function getCookie() {
-    const cookies = document.cookie ? document.cookie.split("; ") : []
-    const prefix = `${cookieName}=`
-    for (let cookie of cookies) {
-      if (cookie.startsWith(prefix))
-        return decodeURIComponent(cookie.substring(prefix.length))
-    }
-  }
-
-  function isKnownCookieValue() {
-    return ["all", "essential"].includes(getCookie())
-  }
-
-  function getConsentToAll() {
-    return getCookie() === "all"
-  }
-
-  function show(el) {
-    el.style.display = ""
-  }
-
-  function hide(el) {
-    if (el) el.style.display = "none"
-  }
-
-  function onAcceptClick(e) {
-    e.preventDefault()
-    setCookie("all")
-    hide(banner)
-    renderModify()
-    injectScripts()
-    renderEmbeds()
-  }
-
-  function onRejectClick(e) {
-    e.preventDefault()
-    setCookie("essential")
-    hide(banner)
-    renderModify()
-  }
-
-  function injectScripts() {
-    for (let cookie of settings.cookies) {
-      let node = injectedScripts[cookie.name]
-      if (!node) {
-        injectedScripts[cookie.name] = node = document.createElement("div")
-        node.dataset.name = cookie.name
-        mainElement.append(node)
-      }
-      node.innerHTML = cookie.script
-      nodeScriptReplace(node)
-    }
-  }
-
-  function initCookieBanner() {
-    mainElement = crel("div", { className: "f3cc" })
-    document.body.append(mainElement)
-
-    if (getConsentToAll()) {
-      injectScripts()
-    }
-
-    if (!isKnownCookieValue()) {
+  let existing
+  if ((existing = qs(".f3cc-modify"))) {
+    existing.addEventListener("click", (e) => {
+      e.preventDefault()
       renderBanner()
-    } else {
-      renderModify()
-    }
+    })
+    return
   }
 
-  const _lsFallback = {},
-    _lsSet = (key, value) => {
-      try {
-        window.localStorage.setItem(key, JSON.stringify(value))
-      } catch (e) {
-        _lsFallback[key] = value
-      }
-    },
-    _lsGet = (key) => {
-      try {
-        return JSON.parse(window.localStorage.getItem(key))
-      } catch (e) {
-        return _lsFallback[key]
-      }
-    }
-
-  const renderEmbeds = (window.f3ccRenderEmbeds = () => {
-    const providers = _lsGet(providerKey) || []
-    const embedNodes = document.querySelectorAll(".f3cc-embed")
-
-    embedNodes.forEach((node) => {
-      const template = node.querySelector("template")
-      const nodesProvider = node.dataset.provider
-
-      if (template && nodesProvider) {
-        if (getConsentToAll() || providers.some((p) => p === nodesProvider)) {
-          const clone = template.content.cloneNode(true)
-          node.closest(".f3cc").replaceWith(clone)
-        }
-      }
-    })
-  })
-
-  function initEmbedClickListener() {
-    document.body.addEventListener("click", (e) => {
-      const button = e.target.closest(".f3cc-button")
-      const node = button && button.closest(".f3cc-embed")
-      if (button && node) {
+  const ppu = settings.privacyPolicyURL
+  const loc = window.location
+  if (
+    settings.buttonModify &&
+    (!ppu || ppu == `${loc.protocol}//${loc.host}${loc.pathname}`)
+  ) {
+    modify = crel("a", {
+      className: "f3cc-button modify",
+      textContent: settings.buttonModify,
+      onclick: (e) => {
         e.preventDefault()
-        const providers = _lsGet(providerKey) || []
-        providers.push(node.dataset.provider)
-        _lsSet(providerKey, providers)
-        renderEmbeds()
-      }
+        hide(modify)
+        renderBanner()
+      },
     })
+    mainElement.append(modify)
+  }
+}
+
+function setCookie(value) {
+  let cookie = `${cookieName}=${value};max-age=31536000;path=/;sameSite=Strict`
+  if (settings.domain) {
+    cookie += `;domain=${settings.domain}`
+  }
+  document.cookie = cookie
+}
+
+function getCookie() {
+  const cookies = document.cookie ? document.cookie.split("; ") : []
+  const prefix = `${cookieName}=`
+  for (let cookie of cookies) {
+    if (cookie.startsWith(prefix))
+      return decodeURIComponent(cookie.substring(prefix.length))
+  }
+}
+
+function isKnownCookieValue() {
+  return ["all", "essential"].includes(getCookie())
+}
+
+function getConsentToAll() {
+  return getCookie() === "all"
+}
+
+function show(el) {
+  el.style.display = ""
+}
+
+function hide(el) {
+  if (el) el.style.display = "none"
+}
+
+function onAcceptClick(e) {
+  e.preventDefault()
+  setCookie("all")
+  hide(banner)
+  renderModify()
+  injectScripts()
+  renderEmbeds()
+}
+
+function onRejectClick(e) {
+  e.preventDefault()
+  setCookie("essential")
+  hide(banner)
+  renderModify()
+}
+
+function injectScripts() {
+  for (let cookie of settings.cookies) {
+    let node = injectedScripts[cookie.name]
+    if (!node) {
+      injectedScripts[cookie.name] = node = document.createElement("div")
+      node.dataset.name = cookie.name
+      mainElement.append(node)
+    }
+    node.innerHTML = cookie.script
+    nodeScriptReplace(node)
+  }
+}
+
+function initCookieBanner() {
+  mainElement = crel("div", { className: "f3cc" })
+  document.body.append(mainElement)
+
+  if (getConsentToAll()) {
+    injectScripts()
   }
 
-  initCookieBanner()
-  renderEmbeds()
-  initEmbedClickListener()
+  if (!isKnownCookieValue()) {
+    renderBanner()
+  } else {
+    renderModify()
+  }
+}
 
-  /*
-  The following functions allow executing scripts added via innerHTML
-  Thanks, https://stackoverflow.com/a/20584396
-  */
-  function nodeScriptReplace(node) {
-    if (nodeScriptIs(node) === true) {
-      node.parentNode.replaceChild(nodeScriptClone(node), node)
-    } else {
-      let i = -1,
-        children = node.childNodes
-      while (++i < children.length) {
-        nodeScriptReplace(children[i])
+const _lsFallback = {},
+  _lsSet = (key, value) => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value))
+    } catch (e) {
+      _lsFallback[key] = value
+    }
+  },
+  _lsGet = (key) => {
+    try {
+      return JSON.parse(window.localStorage.getItem(key))
+    } catch (e) {
+      return _lsFallback[key]
+    }
+  }
+
+const renderEmbeds = (window.f3ccRenderEmbeds = () => {
+  const providers = _lsGet(providerKey) || []
+  const embedNodes = document.querySelectorAll(".f3cc-embed")
+
+  embedNodes.forEach((node) => {
+    const template = qs("template", node)
+    const nodesProvider = node.dataset.provider
+
+    if (template && nodesProvider) {
+      if (getConsentToAll() || providers.some((p) => p === nodesProvider)) {
+        const clone = template.content.cloneNode(true)
+        node.closest(".f3cc").replaceWith(clone)
       }
     }
+  })
+})
 
-    return node
-  }
-
-  function nodeScriptClone(node) {
-    const script = document.createElement("script")
-    script.text = node.innerHTML
-
-    let i = -1,
-      attrs = node.attributes,
-      attr
-    while (++i < attrs.length) {
-      script.setAttribute((attr = attrs[i]).name, attr.value)
+function initEmbedClickListener() {
+  document.body.addEventListener("click", (e) => {
+    const button = e.target.closest(".f3cc-button")
+    const node = button && button.closest(".f3cc-embed")
+    if (button && node) {
+      e.preventDefault()
+      const providers = _lsGet(providerKey) || []
+      providers.push(node.dataset.provider)
+      _lsSet(providerKey, providers)
+      renderEmbeds()
     }
-    return script
+  })
+}
+
+/*
+The following functions allow executing scripts added via innerHTML
+Thanks, https://stackoverflow.com/a/20584396
+*/
+function nodeScriptReplace(node) {
+  if (nodeScriptIs(node) === true) {
+    node.parentNode.replaceChild(nodeScriptClone(node), node)
+  } else {
+    let i = -1,
+      children = node.childNodes
+    while (++i < children.length) {
+      nodeScriptReplace(children[i])
+    }
   }
 
-  function nodeScriptIs(node) {
-    return node.tagName === "SCRIPT"
+  return node
+}
+
+function nodeScriptClone(node) {
+  const script = document.createElement("script")
+  script.text = node.innerHTML
+
+  let i = -1,
+    attrs = node.attributes,
+    attr
+  while (++i < attrs.length) {
+    script.setAttribute((attr = attrs[i]).name, attr.value)
   }
-})()
+  return script
+}
+
+function nodeScriptIs(node) {
+  return node.tagName === "SCRIPT"
+}
 
 /*
 function initEmbedMutationObserver() {
@@ -277,3 +270,7 @@ function initEmbedMutationObserver() {
 }
 initEmbedMutationObserver()
 */
+
+initCookieBanner()
+renderEmbeds()
+initEmbedClickListener()
