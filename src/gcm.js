c77574bea365098fcdf8fb72a5e9e6b2358931d2
York Schickl <ys@feinheit.ch>
2024-06-04 11:52:59 +0200

    actually call gcm api and always set defaults first
    build

--- src/gcm.js
+++ src/gcm.js
@@ -1,19 +1,21 @@
-window.addEventListener("f3cc_consent_granted", () => {
-  updateGAConsentData("granted")
-})
-window.addEventListener("f3cc_consent_denied", () => {
-  updateGAConsentData("denied")
-})
-const updateGAConsentData = (status) => {
-  const dataLayer = (window.dataLayer = window.dataLayer || [])
-  dataLayer.push([
+window.dataLayer = window.dataLayer || []
+function gtag(){dataLayer.push(arguments)}
+function setGAConsentData(action, status) {
+  gtag(
     "consent",
-    "update",
+    action,
     {
       ad_user_data: status,
       ad_personalization: status,
       ad_storage: status,
       analytics_storage: status,
     },
-  ])
+  )
 }
+setGAConsentData("default", "denied")
+window.addEventListener("f3cc_consent_granted", () => {
+  setGAConsentData("update", "granted")
+})
+window.addEventListener("f3cc_consent_denied", () => {
+  setGAConsentData("update", "denied")
+})
